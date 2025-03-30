08b84e84ab5fae7fd165a0a1fe220d1340c1fec1
Aadhar Kulshrestha <aadharkulshrestha3150@gmail.com>
2025-03-30 20:14:58 +0530

    added Apply now and contact us page

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -21,7 +21,7 @@ const navigation: NavigationItem[] = [
     { name: 'Mentors', href: '#blog-section', current: false },
     { name: 'IIC Events', href: '#testimonial-section', current: false },
     { name: 'Insights', href: '#blog-section', current: false },
-    { name: 'Apply Now ', href: '#testimonial-section', current: false },
+    { name: 'Apply Now ', href: '/applyNow', current: false },
     { name: 'Contact Us', href: '/contactUs', current: false },
 ]
 
